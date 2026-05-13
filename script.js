585d20db1838365f79c04a136c6f769e6237b963
Abraao-GOAT <seixasabraao14@gmail.com>
2026-05-13 18:09:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarAjuda() {
+  const ajuda = document.getElementById("ajuda");
+
+  if (ajuda.style.display === "block") {
+    ajuda.style.display = "none";
+  } else {
+    ajuda.style.display = "block";
+  }
+}
